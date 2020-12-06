8267c67cf2e033afe6ad832cd427144c3efd00a7
cccvev <75307540+cccvev@users.noreply.github.com>
2020-12-06 21:17:13 +0200

    Add files via upload

---
+++ Saeed-Mohamed.py
@@ -0,0 +1,69 @@
+import time
+import os
+import datetime
+import compileall
+os.system ("clear")
+from V7xStyle import Animation
+from V7xStyle import Style
+from time import sleep as timeout
+import base64
+import socket
+import sys
+from random import randint
+import re
+from time import sleep
+import pafy
+import requests
+from random import choice
+from N4Tools.Design import Text, Square, Color
+An = Animation
+os.system ("clear")
+def login():
+    os.system("clear")
+    print ("""\033[1;37m                  .-""-.
+    \033[40m          \033[1;37m   / .--. \\
+    \033[40m          \033[1;37m  / /    \\ \\
+    \033[40m          \033[1;37m  | |    | |
+    \033[40m          \033[1;37m  | |.-""-.|
+    \033[40m          \033[1;37m ///`.::::.`\\
+    \033[40m          \033[1;37m||| ::\033[41m/  \\ \033[40m::;
+    \033[40m          \033[1;37m||; ::\033[41m\__/ \033[40m::;
+    \033[40m          \033[1;37m \\ \\.:::::: /
+    \033[40m          \033[1;37m  `=":-..-"`
+    \033[40m      """)
+    print ("\033[41mcall me on whatsapp to get password: +20 110 054 3256  \033[40m")
+    print ('\033[40m ')
+    print ('\033[40m ')
+    print ('\033[40m ')
+    s = (input("\033[1;31m》\033[1;33m》\033[1;32m》\033[1;37m"))
+    sleep(1)
+    An.Loading(
+        text="B#[W#+B#]Y#CHEAK THE PASSWORD.."
+        )
+    if s == 'Saeed':
+        os.system("clear")
+        print ("""
+        \033[40m    \033[1;37m   .-""-.
+        \033[40m    \033[1;37m  / .--. \\
+        \033[40m    \033[1;37m / /    \\ \\
+        \033[40m    \033[1;37m | |    | |
+        \033[40m    \033[1;37m        | |.-""-.
+        \033[40m    \033[1;37m       ///`.::::.`\\
+        \033[40m    \033[1;37m      ||| ::\033[42m/  \\ \033[40m::;
+        \033[40m    \033[1;37m      ||; ::\033[42m\\__/ \033[40m::;
+        \033[40m    \033[1;37m       \\ \\ :::::: /
+        \033[40m    \033[1;37m        `=":-..-"`
+        \033[40m
+
+
+        """)
+        An.Loading(
+            text="G#[W#+G#]W# RunningG# "
+            )
+        os.system("python2 .Saeed.py")
+    else:
+        print (" ")
+        print ("PASSWORD ERROR")
+        os.system("sleep 2")
+        login()
+login()
---
+++ install.py
@@ -0,0 +1,12 @@
+import os
+os.system("""
+cd
+git clone https://github.com/cccvev/modules
+mv modules Saeed-Mohamed
+pkg install python2
+pkg install curl
+pkg install git wget proot
+cd Saeed-Mohamed
+python2 Saeed-Mohamed.py
+
+""")
